e208ba91863720d5d0b0c02b456f280d532c6451
Danny Burkes <dburkes@netable.com>
2009-08-31 11:05:31 -0700

    remove static parse methods from ZipCode

--- lib/people_places_things/zip_code/zip_code.rb
+++ lib/people_places_things/zip_code/zip_code.rb
@@ -1,42 +1,15 @@
 class ZipCode
-  @@SUPPORTED_PARTS = [:base, :plus_four]
-  @@SUPPORTED_PARTS.each {|attr| attr_accessor attr}
-  attr_accessor :raw
+  attr_accessor :base, :plus_four, :raw
   
-  def initialize(parts={})
-    parts.keys.each do |k|
-      send("#{k}=", parts[k]) if @@SUPPORTED_PARTS.include?(k)
-    end
-  end
-  
-  def self.parse(string)
-    tokens = string.strip.match(/^(\d{5})(-\d{4})?$/)[0].split('-') rescue nil
+  def initialize(str)
+    tokens = str.strip.match(/^(\d{5})(-\d{4})?$/)[0].split('-') rescue nil
     raise "Unsupported Format" if !tokens
 
-    parts = {}
-    parts[:base] = tokens.first
-    parts[:plus_four] = tokens[1] rescue nil
-    
-    ret = ZipCode.new parts
-    ret.raw = string
-    ret
+    self.base = tokens.first
+    self.plus_four = tokens[1] rescue nil
   end
   
-  def self.format(parts, fmt)
-    ZipCode.new(parts).to_s(fmt)
+  def to_s
+    [self.base, self.plus_four].compact.join('-')
   end
-  
-  def to_s(fmt = :plus_four)
-    raise "Unsupported Format" if !OUTPUT_FORMATS.include?(fmt)
-
-    case fmt
-      when :base
-        self.base
-        
-      when :plus_four
-        [self.base, self.plus_four].compact.join('-')
-    end
-  end
-  
-  OUTPUT_FORMATS = [:base, :plus_four]
 end
--- spec/zip_code_spec.rb
+++ spec/zip_code_spec.rb
@@ -2,30 +2,26 @@ require 'spec/helper'
 
 describe ZipCode do
   it "should parse base" do
-    zip = ZipCode.parse '30306'
+    zip = ZipCode.new '30306'
     zip.base.should == '30306'
     zip.plus_four.should == nil
   end
 
   it "should parse plus four" do
-    zip = ZipCode.parse '30306-3522'
+    zip = ZipCode.new '30306-3522'
     zip.base.should == '30306'
     zip.plus_four.should == '3522'
   end
-
+  
   it "should throw exception on unsupported parse format" do
-    lambda { ZipCode.parse('303065344') }.should raise_error
+    lambda { ZipCode.new('303065344') }.should raise_error
   end
 
-  it "should format :base" do 
-    ZipCode.parse('30306-3522').to_s(:base).should == '30306'
-  end
-
-  it "should format :plus_four" do 
-    ZipCode.parse('30306-3522').to_s(:plus_four).should == '30306-3522'
+  it "should convert to string" do 
+    ZipCode.new('30306-3522').to_s.should == '30306-3522'
   end
 
   it "should throw exception on unsupported to_s format" do
-    lambda { ZipCode.parse('30306-3522').to_s(:bogus) }.should raise_error
+    lambda { ZipCode.new('30306-3522').to_s(:bogus) }.should raise_error
   end
 end
